4bcf0cda1f158c7c1899efa51ce8a99f7450366c
Ronald <rtrang@uwaterloo.ca>
2020-09-09 22:44:57 -0400

    Made some optimizations to the code

--- src/dataConvert.cpp
+++ src/dataConvert.cpp
@@ -14,36 +14,35 @@ using namespace std;
 
 //create two objects; first to allow for a laserscan -> pointcloud transform, second to describe the transform
 laser_geometry::LaserProjection projector;
-tf::TransformListener listener(ros::Duration(1000));
+tf::TransformListener* listener = NULL;
+std_msgs::Header temp_header;
 
 //create temporary variables to hold the converted pointcloud data. 
 //done because NodeHandle has scoping issues with message variables being created before initialization.
 //also, cannot create a callback function with multiple parameters because they may only have one message type at a time
 
-string temp_header_id;
 int array_size;
-double temp_stamp;
 vector<double> temp_points_x; //arbitrary array size to hold however many points may end up being required
 vector<double> temp_points_y;
 vector<double> temp_points_z;
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-  if(!listener.waitForTransform(msg->header.frame_id, "base_link", msg->header.stamp + ros::Duration().fromSec(msg->ranges.size()*msg->time_increment), ros::Duration(1.0)))
+  if(!listener->waitForTransform(msg->header.frame_id, "base_link", msg->header.stamp + ros::Duration().fromSec(msg->ranges.size()*msg->time_increment), ros::Duration(1.0)))
   {
   } 
   
   //create temporary pointcloud message to hold transformed data
   sensor_msgs::PointCloud cloud;
 
-  projector.transformLaserScanToPointCloud("base_link", *msg, cloud, listener);
+  projector.transformLaserScanToPointCloud("base_link", *msg, cloud, *listener);
 
   //create a variable representing size of array to resize vectors later
   array_size = sizeof(cloud.points)/sizeof(cloud.points[0]);
 
   //start storing cloud values in temporary values
-  temp_header_id = cloud.header.frame_id;
-  temp_stamp = cloud.header.stamp;
+  temp_header.frame_id = cloud.header.frame_id;
+  temp_header.stamp = cloud.header.stamp;
 
   //resize array to match size of cloud array
   temp_points_x.resize(array_size);
@@ -76,6 +75,8 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "convert_data");
   ros::NodeHandle n;
 
+  listener = new(tf::TransformListener);
+
   ros::Subscriber laser_sub = n.subscribe("scan", 50, laserCallback);
   ros::Subscriber odom_sub = n.subscribe("odom", 50, odomCallback);
 
@@ -83,16 +84,16 @@ int main(int argc, char** argv)
 
   //create a proper pointcloud message and repopulate with temp data
   sensor_msgs::PointCloud data;
-  data.header.frame_id = temp_header_id;
-  data.header.stamp = temp_stamp;
+  data.header.frame_id = temp_header.frame_id;
+  data.header.stamp = temp_header.stamp;
 
   data.points.resize(array_size);
 
   for (int i{}; i < array_size; ++i)
   {
-    data.points[i].x = temp_points.x[i];
-    data.points[i].y = temp_points.y[i];
-    data.points[i].z = temp_points.z[i];
+    data.points[i].x = temp_points_x[i];
+    data.points[i].y = temp_points_y[i];
+    data.points[i].z = temp_points_z[i];
   }
 
   data_pub.publish(data);
