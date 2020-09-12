451da080b7cc18942171f6624643a26fde12301c
Ronald <rtrang@uwaterloo.ca>
2020-09-11 21:59:18 -0400

    added a launch file for convenience

--- src/dataConvert.cpp
+++ src/dataConvert.cpp
@@ -15,16 +15,18 @@ using namespace std;
 //create two objects; first to allow for a laserscan -> pointcloud transform, second to describe the transform
 laser_geometry::LaserProjection projector;
 tf::TransformListener* listener = NULL;
-std_msgs::Header temp_header;
+sensor_msgs::PointCloud temp_data;
 
 //create temporary variables to hold the converted pointcloud data. 
 //done because NodeHandle has scoping issues with message variables being created before initialization.
 //also, cannot create a callback function with multiple parameters because they may only have one message type at a time
 
-int array_size;
+int array_size = 0;
+/*
 vector<double> temp_points_x; //arbitrary array size to hold however many points may end up being required
 vector<double> temp_points_y;
 vector<double> temp_points_z;
+*/
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
@@ -41,21 +43,21 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
   array_size = sizeof(cloud.points)/sizeof(cloud.points[0]);
 
   //start storing cloud values in temporary values
-  temp_header.frame_id = cloud.header.frame_id;
-  temp_header.stamp = cloud.header.stamp;
+  temp_data.header.frame_id = cloud.header.frame_id;
+  temp_data.header.stamp = cloud.header.stamp;
 
   //resize array to match size of cloud array
-  temp_points_x.resize(array_size);
-  temp_points_y.resize(array_size);
-  temp_points_z.resize(array_size);
+  //temp_points_x.resize(array_size);
+  //temp_points_y.resize(array_size);
+  //temp_points_z.resize(array_size);
+  temp_data.points.resize(array_size);
 
   //transfer all the values in array
   for (int i{}; i < array_size; ++i)
   {
-    temp_points_x[i] = cloud.points[i].x;
-    temp_points_y[i] = cloud.points[i].y;
-    temp_points_z[i] = cloud.points[i].z;
+    temp_data.points[i] = cloud.points[i];
   }
+  
 }
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
@@ -63,9 +65,9 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
   //add odometry data to the temporary array
   for (int i{}; i < array_size; ++i)
   {
-    temp_points_x[i] += msg->pose.pose.position.x;
-    temp_points_y[i] += msg->pose.pose.position.y;
-    temp_points_z[i] += msg->pose.pose.position.z;
+    temp_data.points[i].x += msg->pose.pose.position.x;
+    temp_data.points[i].y += msg->pose.pose.position.y;
+    temp_data.points[i].z += msg->pose.pose.position.z;
   }
 }
 
@@ -78,26 +80,27 @@ int main(int argc, char** argv)
   listener = new(tf::TransformListener);
 
   ros::Subscriber laser_sub = n.subscribe("scan", 50, laserCallback);
+  ros::spinOnce();
   ros::Subscriber odom_sub = n.subscribe("odom", 50, odomCallback);
+  ros::spinOnce();
 
   ros::Publisher data_pub = n.advertise<sensor_msgs::PointCloud>("pointData", 50);
 
   //create a proper pointcloud message and repopulate with temp data
   sensor_msgs::PointCloud data;
-  data.header.frame_id = temp_header.frame_id;
-  data.header.stamp = temp_header.stamp;
+  data.header.frame_id = temp_data.header.frame_id;
+  data.header.stamp = temp_data.header.stamp;
 
   data.points.resize(array_size);
 
   for (int i{}; i < array_size; ++i)
   {
-    data.points[i].x = temp_points_x[i];
-    data.points[i].y = temp_points_y[i];
-    data.points[i].z = temp_points_z[i];
+    data.points[i].x = temp_data.points[i].x;
+    data.points[i].y = temp_data.points[i].y;
+    data.points[i].z = temp_data.points[i].z;
   }
 
   data_pub.publish(data);
-  ros::spin();
+  //ros::spin();
 
-  return 0;
 }
